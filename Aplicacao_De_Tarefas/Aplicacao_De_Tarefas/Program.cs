0efe72ece8231c8b79e11c765aa36968d5394a09
Paulo Ricardo <pricardoporfirio2001@gmail.com>
2024-07-26 09:22:41 -0300

    Iniciado Projeto de Treino Blazor - Controle de Tarefas

---
+++ Aplicacao_De_Tarefas/Aplicacao_De_Tarefas/Program.cs
@@ -0,0 +1,31 @@
+using Aplicacao_De_Tarefas.Client.Pages;
+using Aplicacao_De_Tarefas.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Aplicacao_De_Tarefas.Client._Imports).Assembly);
+
+app.Run();
